361c9ae63c446cee3d3e4e8ddddeda3a8b5dea5a
Marcel Suter <marcel.suter@bzz.ch>
2023-07-22 07:12:11 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 def main_while():
-    # Schleife mit Bedingung OHNE break und continue
-    pass
+    number = int(input('Give a number:\n'))
+    while number != 0:
+        if number > 0:
+            print(number * number)
+        elif number < 0:
+            print('Unsuitable number')
+        number = int(input('Give a number:\n'))
 
 
 def main_break():
-    # Endlosschleife mit break
-    pass
+    while True:
+        number = int(input('Give a number:\n'))
+        if number > 0:
+            print(number * number)
+        elif number == 0:
+            break
+        else:
+            print('Unsuitable number')
 
 
 def main_continue():
-    # Schleife mit Bedingung und continue
-    pass
+    number = 1
+    while number != 0:
+        number = int(input('Give a number:\n'))
+        if number > 0:
+            print(number * number)
+        elif number == 0:
+            continue
+        else:
+            print('Unsuitable number')
+
+
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -6,4 +6,44 @@ def test_while_1(capsys, monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main.main_while()
     captured = capsys.readouterr()
-    assert captured.out == 'The number is positive.\n'
+    assert captured.out == ''
+
+
+def test_while_2(capsys, monkeypatch):
+    inputs = iter([4, -3, 0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main_while()
+    captured = capsys.readouterr()
+    assert captured.out == '16\nUnsuitable number\n'
+
+
+def test_break_1(capsys, monkeypatch):
+    inputs = iter([0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main_while()
+    captured = capsys.readouterr()
+    assert captured.out == ''
+
+
+def test_break_2(capsys, monkeypatch):
+    inputs = iter([4, -3, 0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main_while()
+    captured = capsys.readouterr()
+    assert captured.out == '16\nUnsuitable number\n'
+
+
+def test_continue_1(capsys, monkeypatch):
+    inputs = iter([0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main_while()
+    captured = capsys.readouterr()
+    assert captured.out == ''
+
+
+def test_continue_2(capsys, monkeypatch):
+    inputs = iter([4, -3, 0])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main_while()
+    captured = capsys.readouterr()
+    assert captured.out == '16\nUnsuitable number\n'
